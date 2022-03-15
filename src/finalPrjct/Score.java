764dd888236faa1c2a4194cf20e678cbc9eebbb2
DiMa HajajLah <79136459+dimahajajlah@users.noreply.github.com>
2022-03-15 17:05:45 +0200

    -

--- src/finalPrjct/Score.java
+++ src/finalPrjct/Score.java
@@ -5,6 +5,11 @@ import java.awt.Rectangle;
 
 public class Score extends Rectangle {
 	
+	static int gameWidth; //game Width
+	static int gameHeight; //game Height
+	int player1;
+	int player2;
+	
 	Score(){
 		
 	}
