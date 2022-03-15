4f791fa402714cedb79f2d80204b6271e54f3344
DiMa HajajLah <79136459+dimahajajlah@users.noreply.github.com>
2022-03-15 17:18:32 +0200

    -

--- src/finalPrjct/GameFrame.java
+++ src/finalPrjct/GameFrame.java
@@ -1,5 +1,7 @@
 package finalPrjct;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
@@ -7,6 +9,14 @@ public class GameFrame extends JFrame {
 	GamePanel panel;
 	GameFrame(){
 		panel = new GamePanel();
+		this.add(panel);
+		this.setTitle("Pong Game");
+		this.setResizable(false);
+		this.setBackground(Color.blue);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null); //it's going to appear in the middle of the screen 
 	}
 
 }
